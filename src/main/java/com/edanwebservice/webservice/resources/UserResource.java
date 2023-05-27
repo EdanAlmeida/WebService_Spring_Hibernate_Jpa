43e8f4c7884a2ccc7e3b0ff9e2fa6f9b5b842e09
EdanAlmeida <edluisalmeida@gmail.com>
2023-05-27 14:38:17 -0300

    Service layer - component registration

--- src/main/java/com/edanwebservice/webservice/resources/UserResource.java
+++ src/main/java/com/edanwebservice/webservice/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.edanwebservice.webservice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edanwebservice.webservice.entities.User;
+import com.edanwebservice.webservice.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Edan", "edan@email.com", "42984888888", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = userService.findAll();
+		return ResponseEntity.ok().body(userList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
