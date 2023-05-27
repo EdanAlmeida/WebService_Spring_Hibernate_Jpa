c9c96e0bf9c614946cfc85de88f078784e455752
EdanAlmeida <edluisalmeida@gmail.com>
2023-05-27 16:10:48 -0300

    Order - Instant - ISO 8601

--- src/main/java/com/edanwebservice/webservice/entities/User.java
+++ src/main/java/com/edanwebservice/webservice/entities/User.java
@@ -1,12 +1,17 @@
 package com.edanwebservice.webservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,12 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	/*Ao carregar um objeto que tem associação para muitos (Cliente - pedido, o JPA não carrega o objeto do lado do muitos por padrão
+	 * Lazy loading*/
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -73,6 +84,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -90,4 +105,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
---
+++ src/main/java/com/edanwebservice/webservice/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.edanwebservice.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edanwebservice.webservice.entities.Order;
+import com.edanwebservice.webservice.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll() {
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> order = orderRepository.findById(id);
+		return order.get();
+	}
+	
+}
