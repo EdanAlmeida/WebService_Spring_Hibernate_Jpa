6dcf51d32b89e3dfcb5d607bef391dcfa9661357
EdanAlmeida <edluisalmeida@gmail.com>
2023-05-27 21:41:17 -0300

    OrderItem, many-tomany, association with extra attributes

--- src/main/java/com/edanwebservice/webservice/config/TesteConfig.java
+++ src/main/java/com/edanwebservice/webservice/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.edanwebservice.webservice.entities.Category;
 import com.edanwebservice.webservice.entities.Order;
+import com.edanwebservice.webservice.entities.OrderItem;
 import com.edanwebservice.webservice.entities.Product;
 import com.edanwebservice.webservice.entities.User;
 import com.edanwebservice.webservice.entities.enums.OrderStatus;
 import com.edanwebservice.webservice.repositories.CategoryRepository;
+import com.edanwebservice.webservice.repositories.OrderItemRepository;
 import com.edanwebservice.webservice.repositories.OrderRepository;
 import com.edanwebservice.webservice.repositories.ProductRepository;
 import com.edanwebservice.webservice.repositories.UserRepository;
@@ -32,7 +34,10 @@ public class TesteConfig implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	
 	@Autowired 
-	ProductRepository productRepository;
+	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -78,6 +83,13 @@ public class TesteConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		/*Salvando os Pedidos*/
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	
